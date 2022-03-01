ee212695adfb3427bce2f970e044bea5fc6c83ec
Admin <aliyarnadeem@gmail.com>
2022-03-01 17:46:27 +0530

    day 16

---
+++ jdbc demo/src/jdbcdemo.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class jdbcdemo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");// step 2  class loader 
+		
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college","root", "Nadeem@123");//step 3 create connection
+		
+		Statement st = con.createStatement();// step 4
+		//String query = "create table student(ID int primary key,SName varchar(20), SMarks int)";//step 6
+		//String query1 = "insert into student values(1,'Nadeem',24)";
+		//String query2 = "insert into student values(2,'hassan',25),(3,'jawad',30)";
+		//String query3 = "UPDATE student SET SName='aftab', SMarks=40 WHERE ID=2";
+		String query4 = "DELETE FROM student WHERE ID=2";
+		//st.executeUpdate(query);
+		//st.executeUpdate(query1);
+		//st.executeUpdate(query2);
+		//st.executeUpdate(query3);
+		st.executeUpdate(query4);
+		
+		System.out.println("Student Table created successfully");
+		
+		System.out.println("Row inserted successfully");
+		
+	st.close();
+	con.close();
+	}
+
+}
